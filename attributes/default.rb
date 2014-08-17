468991f418246efee8f1700fc4b9e28444777adb
Umang Chouhan <uchouhan@optimiscorp.com>
2014-08-17 23:57:02 +0530

    Adding rbenv global version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
+node.default['rbenv']['global'] = '2.1.2'
 node.default['rbenv']['gem_prefix'] = node['rbenv']['global'].include?('2.1') ? '2.1.0' : '1.9.1'
 node.default['passenger']['version'] = '4.0.48'
